4a772897efda223dabcf12744ac78028fd103083
lcd1232 <8745863+lcd1232@users.noreply.github.com>
2023-05-31 15:48:57 +0400

    Fix config dir

--- main.py
+++ main.py
@@ -23,8 +23,7 @@ class Plugin:
         global config
         if config:
             return config
-        file_path: str = os.path.join(decky_plugin.DECKY_PLUGIN_DIR, "config.json")
-        # file_path: str = os.path.join("/tmp", "config.json")
+        file_path: str = os.path.join(decky_plugin.DECKY_PLUGIN_SETTINGS_DIR, "config.json")
         headers: dict = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
             "(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
